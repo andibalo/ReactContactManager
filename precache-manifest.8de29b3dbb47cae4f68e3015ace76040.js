e3ab7c6ed5b551338f60f08d4cd41e6162807c08
andi balo <andibalo213@gmail.com>
2019-11-22 03:53:05 +0700

    Updates

---
+++ precache-manifest.8de29b3dbb47cae4f68e3015ace76040.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14ab317561e895588746b0fa0bd9b502",
+    "url": "/ReactContactManager/index.html"
+  },
+  {
+    "revision": "77b88e6e22127d248faa",
+    "url": "/ReactContactManager/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "633655ab5db27bb8f93d",
+    "url": "/ReactContactManager/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "77b88e6e22127d248faa",
+    "url": "/ReactContactManager/static/js/2.e17c8ce1.chunk.js"
+  },
+  {
+    "revision": "633655ab5db27bb8f93d",
+    "url": "/ReactContactManager/static/js/main.b68e8a73.chunk.js"
+  },
+  {
+    "revision": "2f1bd3596ed63806bf9c",
+    "url": "/ReactContactManager/static/js/runtime-main.91274915.js"
+  }
+]);
